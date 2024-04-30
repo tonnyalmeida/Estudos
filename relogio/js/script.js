9b23dd7263566bcafe6d9cff451c00032d75fba8
TonnyAlmeidaa <166747870+TonnyAlmeidaa@users.noreply.github.com>
2024-04-30 19:05:12 -0300

    reloginho lindo

--- relogio/js/script.js
+++ relogio/js/script.js
@@ -1,6 +1,7 @@
 const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
+const wall = document.getElementById('fundo')
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -8,11 +9,41 @@ const relogio = setInterval(function time() {
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
-    if(hr < 10) hr = '0' + hr;
-    if(min < 10) min = '0' + min;
-    if(sec < 10) sec = '0' + sec;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+
+    if (hr >= 0 && hr <= 5) {
+        hr = '0' + hr;
+        wall.style.backgroundImage = "url('imagens/foto-madrugada.jpg')"
+        document.body.style.background = '#5D559A'
+    } else if (hr >= 6 && hr < 10) {
+        hr = '0' + hr;
+        wall.style.backgroundImage = "url('imagens/foto-dia.jpg')"
+        document.body.style.background = '#509ED7'
+    } else if (hr >= 10 && hr < 15) {
+        wall.style.backgroundImage = "url('imagens/foto-dia.jpg')"
+        document.body.style.background = '#509ED7'
+    } else if (hr >= 15 && hr < 18) {
+        wall.style.backgroundImage = "url('imagens/foto-tarde.jpg')"
+        document.body.style.background = '#FCBC68'
+    } else {
+        wall.style.backgroundImage = "url('imagens/foto-noite.jpg')"
+        document.body.style.background = '#8E6DEF'
+    }
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = sec;
 })
+
+
+
+
+
+/*
+
+if(hr < 10) hr = '0' + hr;
+if(min < 10) min = '0' + min;
+if(sec < 10) sec = '0' + sec;
+
+*/
